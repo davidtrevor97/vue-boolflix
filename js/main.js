c2a1788b99344aaea4a153805927fe632b448fec
david cortesi <david.cortesi.097@gmail.com>
2020-11-25 21:59:19 +0100

    commit 4

--- js/main.js
+++ js/main.js
@@ -5,35 +5,34 @@
 // 	return voto / 2;
 // }
 //180223961313acb0a2739bfabe754841
-// <i  v-for="n in 5 - getVote(voto)">
+// <i  v-for="n in 5 - getStars(voto)">
 const app = new Vue({
   el:".app",
   data:{
     actualSearch: "",
+    picturesArray: [],
   },
   methods:{
     movieSrc(){
       axios.get("https://api.themoviedb.org/3/search/movie" , {
         params: {
           api_key: "180223961313acb0a2739bfabe754841",
-          query: "revenant",
+          query: this.actualSearch,
         }
       })
       .then( result => {
-      console.log(result.data.results);
+      this.picturesArray = result.data.results;
+      return result.data.results;
     })
-    }
-  },
-  computed:{
-    movieSearch(){
-      axios.get("https://api.themoviedb.org/3/movie/76341?api_key=180223961313acb0a2739bfabe754841&query=ritorno+al+futuro")
-      .then( result => {
-        return result.filter( (movie) => {
-          return movie.title.match(this.actualSearch)
-        })
-      })
-      .catch( result => {});
-
-    }
+    .catch( error => {
+      console.log(error)
+      this.errored = true
+    } );
+   },
+   getStars(picturesArray){
+     for (var i = 0; i < this.picturesArray.length; i++) {
+       return Math.floor(this.picturesArray[i].vote_average  / 2);
+     }
+   },
   }
 });
