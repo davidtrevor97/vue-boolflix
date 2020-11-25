3b648066f9c34b994bb8bbf7051e5cc431e67916
david cortesi <david.cortesi.097@gmail.com>
2020-11-25 17:19:02 +0100

    commit 3

--- js/main.js
+++ js/main.js
@@ -12,13 +12,21 @@ const app = new Vue({
     actualSearch: "",
   },
   methods:{
-    movieSearch(){
-
+    movieSrc(){
+      axios.get("https://api.themoviedb.org/3/search/movie" , {
+        params: {
+          api_key: "180223961313acb0a2739bfabe754841",
+          query: "revenant",
+        }
+      })
+      .then( result => {
+      console.log(result.data.results);
+    })
     }
   },
   computed:{
     movieSearch(){
-      axios.get("https://api.themoviedb.org/3/movie/76341?api_key=180223961313acb0a2739bfabe754841")
+      axios.get("https://api.themoviedb.org/3/movie/76341?api_key=180223961313acb0a2739bfabe754841&query=ritorno+al+futuro")
       .then( result => {
         return result.filter( (movie) => {
           return movie.title.match(this.actualSearch)
