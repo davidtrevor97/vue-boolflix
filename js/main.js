c184c1898d98083da359fda6889a7ced655b20d6
david cortesi <david.cortesi.097@gmail.com>
2020-11-26 15:15:29 +0100

    commit 6

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@
 // 	return voto / 2;
 // }
 //180223961313acb0a2739bfabe754841
-// <i  v-for="n in 5 - getStars(voto)">
+// this.results.concat(res)
 const app = new Vue({
   el:".app",
   data:{
     actualSearch: "",
+    filmsArray: [],
+    pictureUrl: "",
     picturesArray: [],
   },
   methods:{
@@ -21,18 +23,45 @@ const app = new Vue({
         }
       })
       .then( result => {
-      this.picturesArray = result.data.results;
+      this.filmsArray = result.data.results;
       return result.data.results;
     })
     .catch( error => {
       console.log(error)
       this.errored = true
     } );
-   },
-   getStars(picturesArray){
-     for (var i = 0; i < this.picturesArray.length; i++) {
-       return Math.floor(this.picturesArray[i].vote_average  / 2);
-     }
+  },
+  seriesSrc(){
+    axios.get("https://api.themoviedb.org/3/search/tv" , {
+      params: {
+        api_key: "180223961313acb0a2739bfabe754841",
+        query: this.actualSearch,
+      }
+    })
+    .then( result => {
+    this.filmsArray = this.results.concat(result);
+    return this.results.concat(result);
+  })
+  .catch( error => {
+    console.log(error)
+    this.errored = true
+  } );
+  },
+    imgSrc(){
+      for (var i = 0; i < this.filmsArray.length; i++) {
+      pictureUrl =  this.filmsArray[i].poster_path;
+      axios.get("https://image.tmdb.org/t/p/"+"w300"+pictureUrl)
+      .then( result => {
+        console.log(result.data);
+    })
+    .catch( error => {
+      console.log(error)
+      this.errored = true
+    } );
+  }
+  },
+   getStars(vote){
+       return Math.floor(vote / 2);
    },
   }
 });
